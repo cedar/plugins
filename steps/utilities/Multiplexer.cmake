8eb64b2067094b4a6772373e6cd9bdf534ddf15c
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2016-02-08 17:49:10 +0100

    Added the option to moc step headers as well as some steps that use it.

---
+++ steps/utilities/Multiplexer.cmake
@@ -0,0 +1,18 @@
+# all options that are specified with a keyword are optional (e.g., MOC
+DECLARE_STEP(cedar::proc::steps::Multiplexer # the class name as used in the code; will be added to the plugin.cpp
+#  ADDITIONAL_SOURCES my_source.cpp
+#  ICON             "bla.svg" # the icon to be used, if any
+             CATEGORY         "Utilities" # the category in which the step appears in cedar
+             MOC
+#  ADDITIONAL_MOC_HEADERS my_special_field.h # we may also want to moc some headers for the step (usually just one)
+#  AUTHOR           Some Guy
+#  DESCRIPTION      "This does something, I swear!"
+#  MAINTAINER       "Dr. Whoeverisresponsible"
+#  REQUIRES         # section for specifying different requirements
+#    CEDAR          DEVELOPMENT # this step requires cedar development; maybe also/instead something like VERSION >= 5.0.0, and development always has a higher version than release?
+#    LIBS           KEYPOINTS # an external library needed by this step
+#  DEPRECATED       "Don't use this step any more."
+#  DEPRECATED_NAMES
+#  OS               UNIX WINDOWS
+#  DEPENDS_ON       cedar::proc::steps::SomeOtherStep
+)
