79bf80c8f9b1f12d0f05d86dd5360d4bf5516501
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2016-02-09 13:45:04 +0100

    Warnings about missing descriptions are now printed even if the step is not part of the buildset. They also contain the name of the maintainer.

--- steps/sources/SpatialPattern.cmake
+++ steps/sources/SpatialPattern.cmake
@@ -1,4 +1,6 @@
 DECLARE_STEP(cedar::proc::steps::SpatialPattern
   CATEGORY "Sources"
   MOC
+  MAINTAINER "Mathis Richter"
+  DESCRIPTION "Outputs a matrix of synaptic weights that correspond to a spatial relational template (e.g., \"to the left of\")."
 )
