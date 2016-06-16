4a7c7a4b3725afea2db1bc306e3d00cf1d27b320
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2016-06-16 12:07:41 +0200

    Added a LabelWMAnnotator step from the object recognition plugin.

---
+++ steps/utilities/LabelWMAnnotator.cmake
@@ -0,0 +1,7 @@
+DECLARE_STEP(cedar::proc::steps::LabelWMAnnotator
+             MOC
+             CATEGORY         "Utilities"
+             DESCRIPTION      "Takes a yarp-friendly label WM representation and puts labels into an image."
+             MAINTAINER       "Oliver Lomp"
+             DEPRECATED_NAME  utilities::LabelWMAnnotator
+            )
