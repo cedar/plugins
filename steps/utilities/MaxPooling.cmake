8a02985be919cfea59151a91910dfd07fdca4e8f
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2016-06-16 12:23:06 +0200

    Added the MaxPooling step from the ObjectRecognitionPlugin.

---
+++ steps/utilities/MaxPooling.cmake
@@ -0,0 +1,7 @@
+DECLARE_STEP(cedar::proc::steps::MaxPooling
+             CATEGORY         "Utilities"
+             MOC
+             DESCRIPTION      "Subsamples a matrix by taking the maximum over rectangular regions."
+             MAINTAINER       "Oliver Lomp"
+             DEPRECATED_NAMES utilities::MaxPooling
+            )
