d824aa7a518375d84a062dc5f8ca76d29a98b7d2
Mathis Richter <mathis.richter@ini.rub.de>
2018-10-02 11:08:31 +0200

    Removed steps SerialOrderRecruiting and ShiftedAddition, because both are now in cedar.

--- steps/dynamics/SerialOrderRecruiting.cmake
+++
@@ -1,6 +0,0 @@
-DECLARE_STEP(cedar::dyn::SerialOrderRecruiting
-             MOC
-             CATEGORY         "Dynamics"
-             DESCRIPTION      "An Extension of the Serial Order Step, that allows a dynamic growth of serial order nodes and optionally a recruition of additional Groups toghether with the RecruitGroupsScript."
-             MAINTAINER       "Jan Tekülve"
-            )
--- steps/utilities/ShiftedAddition.cmake
+++
@@ -1,5 +0,0 @@
-DECLARE_STEP(cedar::proc::steps::ShiftedAddition
-  CATEGORY "Arrays"
-  MOC
-  MAINTAINER "Mathis Richter"
-)
